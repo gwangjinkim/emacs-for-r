30cdd88bf97f272ef53bd6fc08774647f11944e9
Gwang-Jin Kim <gwang.jin.kim.phd@gmail.com>
2022-04-16 11:24:42 +0200

    correct init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -333,3 +333,19 @@
 ;; alias remacs="conda activate emacs && env ORIG_HOME=$HOME HOME=/emacs/emacs-for-r PATH=$HOME/miniconda3/envs/r/bin:$PATH emacs &"
 
 (setenv "HOME" (getenv "ORIG_HOME"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ess slime which-key use-package try tabbar ox-reveal org-bullets htmlize counsel conda company ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
